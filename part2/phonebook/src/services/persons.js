a853b17df6c0d33da3fd5f50cca9b0c78c7b4a22
Emilio Iglesias <emilioiglesiasmayor@gmail.com>
2023-12-01 17:05:34 +0100

    ✨ part 3 finished

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+//const baseUrl = 'https://localhost:3001/persons'
+const baseUrl = 'https://api-part3.fly.dev/api/persons'
 
 const getAll = () => {
   return axios
--- part3/index.js
+++ part3/index.js
@@ -1,42 +1,42 @@
 const express = require('express')
 const morgan = require('morgan')
-const app = express()
+const cors = require('cors')
 
-app.use(express.json());
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
 
 // Configurar Morgan con un formato personalizado
 morgan.token('request-body', (req) => JSON.stringify(req.body))
 
-const logFormat = ':method :url :status - :response-time ms :request-body';
+const logFormat = ':method :url :status - :response-time ms :request-body'
 
-app.use(morgan(logFormat));
+app.use(morgan(logFormat))
 // app.use(morgan)
 
-
 let persons = [
-  { 
-      id: 1,
-      name: "Arto Hellas", 
-      number: "040-123456"
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456'
   },
-  { 
-      id: 2,
-      name: "Ada Lovelace", 
-      number: "39-44-5323523"
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523'
   },
-  { 
-      id: 3,
-      name: "Dan Abramov", 
-      number: "12-43-234345"
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345'
   },
-  { 
-      id: 4,
-      name: "Mary Poppendieck", 
-      number: "39-23-6423122"
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122'
   }
 ]
-
-
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -66,14 +66,14 @@ app.get('/api/persons/:id', (request, response) => {
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   persons = persons.filter(person => person.id !== id)
-  
+
   response.status(204).end()
 })
 
 app.post('/api/persons/', (request, response) => {
   // const maxId = persons.length > 0 ? Math.max(...persons.map(person => person.id)) : 1
   const body = request.body
-  
+
   if (!body.name || !body.number) {
     return response.status(400).json({ error: 'content missing' })
   }
@@ -83,9 +83,9 @@ app.post('/api/persons/', (request, response) => {
   }
 
   const newPerson = {
-    id: Math.floor(Math.random()*10000),
+    id: Math.floor(Math.random() * 10000),
     name: body.name,
-    number: body.number,
+    number: body.number
   }
 
   persons = persons.concat(newPerson)
@@ -98,8 +98,7 @@ app.post('/api/persons/', (request, response) => {
   }
 })
 
-
-const PORT = 3001
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
