cf0370f5341868b7dbe0482b232ddf6fe96e7e40
Emilio <emilioiglesiasmayor@gmail.com>
2024-03-16 18:03:05 +0100

    hasta ejercicio 6.5

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -21,10 +21,12 @@ const App = () => {
     setNewAnecdote(''); // Limpia el input después de crear la anécdota
   };
 
+  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes);
+
   return (
     <>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {sortedAnecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
