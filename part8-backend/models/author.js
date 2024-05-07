ee551d4ddbfc1f108c4adaa2cd342578dab7fc2b
Emilio <emilioiglesiasmayor@gmail.com>
2024-05-07 12:37:12 +0200

    hasta ejercicio 8.16

--- part8-backend/models/author.js
+++ part8-backend/models/author.js
@@ -1,17 +1,9 @@
 const mongoose = require("mongoose");
 
-const authorSchema = new mongoose.Schema({
+const schema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 3 },
   born: Number,
   // bookCount is calculated
 });
 
-authorSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
-
-module.exports = mongoose.model("Author", authorSchema);
+module.exports = mongoose.model("Author", schema);
